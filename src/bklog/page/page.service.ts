c3c2636da06a4bbed72d7754de8c6c66fe85d0ff
junhee <wjunhee102@gmail.com>
2021-03-16 21:35:47 +0900

    refactor: getPageList query 수정

--- src/bklog/page/page.service.ts
+++ src/bklog/page/page.service.ts
@@ -1,16 +1,33 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { PageRepository } from './repositories/page.repository';
-import { InfoToFindPage, RequiredPageInfo, PageInfoList } from './page.type';
+import { InfoToFindPage, RequiredPageInfo, PageInfoList, PageUserInfo } from './page.type';
 import { Page } from 'src/entities/bklog/page.entity';
 import { Token } from 'src/util/token.util';
-import { MoreThan, MoreThanOrEqual, Like } from 'typeorm';
-import { UserProfile } from 'src/entities/user/user-profile.entity';
+import { MoreThanOrEqual } from 'typeorm';
 
 @Injectable()
 export class PageService {
   constructor(
     private readonly pageRepository: PageRepository
   ){}
+
+  /**
+   * 
+   * @param requiredPageInfo 
+   */
+  private async insertPage(requiredPageInfo: RequiredPageInfo): Promise<Page> {
+    const page: Page = await this.pageRepository.create();
+
+    page.id = Token.getUUID();
+    page.userProfile = requiredPageInfo.userProfile;
+    page.userId = requiredPageInfo.userId;
+    page.title = requiredPageInfo.title;
+    page.disclosureScope = requiredPageInfo.disclosureScope;
+
+    await this.savePage(page);
+
+    return await this.findOnePage({id: page.id});
+  }
 
   /**
    * page 정보 찾기
@@ -22,12 +39,20 @@ export class PageService {
     });
   }
 
+  /**
+   * 
+   * @param pageInfo 
+   */
   private async findPage(pageInfo: InfoToFindPage): Promise<Page[]> {
     return await this.pageRepository.find({
       where: pageInfo
     });
   }
 
+  /**
+   * 
+   * @param page 
+   */
   private async savePage(page: Page) {
     try {
       await this.pageRepository.save(page);
@@ -40,27 +65,10 @@ export class PageService {
     }
   }
 
-  private async insertPage(requiredPageInfo: RequiredPageInfo): Promise<Page | null> {
-    try {
-      const page: Page = await this.pageRepository.create();
-
-      page.id = Token.getUUID();
-      page.userProfile = requiredPageInfo.userProfile;
-      page.userId = requiredPageInfo.userId;
-      page.title = requiredPageInfo.title;
-      page.disclosureScope = requiredPageInfo.disclosureScope;
-
-      this.savePage(page);
-      console.log(page);
-      return page;
-      
-    } catch(e) {
-      Logger.error(e);
-
-      return null;
-    }
-  }
-
+  /**
+   * 
+   * @param page 
+   */
   private async deletePage(page: Page): Promise<boolean> {
     try {
       await this.pageRepository.delete(page);
@@ -73,16 +81,56 @@ export class PageService {
     }
   }
 
-  public async createPage(requiredPageInfo: RequiredPageInfo): Promise<string | null> {
+  /**
+   * 
+   * @param requiredPageInfo 
+   */
+  public async createPage(requiredPageInfo: RequiredPageInfo): Promise<Page> {
     const page: Page | null = await this.insertPage(requiredPageInfo);
     if(page) {
-      console.log(page);
-      return page.id;
+      return page;
     } else {
       return null;
     }
   }
 
+  /**
+   * 
+   * @param id 
+   */
+  public async getPage(id: string): Promise<Page> {
+
+    console.log(id);
+    const page: Page = await this.pageRepository
+      .createQueryBuilder("page")
+      .where({
+        id
+      })
+      .leftJoinAndSelect("page.userProfile", "user-profile")
+      .leftJoinAndSelect("page.blockList", "block")
+      .leftJoinAndSelect("page.versionList", "page-version")
+      .leftJoinAndSelect("block.property","block-property")
+      .getOne();
+  
+    console.log("asdasd", page);
+
+    if(!page) {
+      return null
+    }
+
+    page.lastAccessDate = new Date(Date.now());
+    page.views = Number(page.views) + 1;
+
+    await this.savePage(page);
+
+    return page;
+  }
+
+  /**
+   * 
+   * @param pageId 
+   * @param userId 
+   */
   public async removePage(pageId: string, userId: string): Promise<boolean> {
     const page: Page | null  = await this.findOnePage({id: pageId, userId});
 
@@ -95,34 +143,55 @@ export class PageService {
     return false;
   }
 
-  public async findPublicPageList(profileId, scope:number = 4): Promise<PageInfoList[]> {
+  /**
+   * 
+   * @param profileId 
+   * @param scope 
+   */
+  public async findPublicPageList({ id, penName }: PageUserInfo, scope:number = 4): Promise<PageInfoList[]> {
     if(!scope) {
       console.log(scope);
       return null;
     }
 
-    if(profileId) { 
-      const pageList: Page[] = await this.pageRepository.find({
-        where: {
-          profileId,
-          disclosureScope: MoreThanOrEqual(4),
-        },
-        order: {
-          disclosureScope: "ASC",
-          title: "ASC"
-        }
-      });
+    if(id || penName) { 
+      // const pageList: Page[] = await this.pageRepository.find({
+      //   relations: ["userProfile"],
+      //   where: {
+      //     userProfile: {
+      //       id: pageUserInfo.id,
+      //       penName: pageUserInfo.penName
+      //     },
+      //     disclosureScope: MoreThanOrEqual(0),
+      //   },
+      //   order: {
+      //     disclosureScope: "ASC",
+      //     title: "ASC"
+      //   }
+      // });
 
-      console.log(pageList);
+      
+
+      const pageList: Page[] = await this.pageRepository
+        .createQueryBuilder("page")
+        .leftJoinAndSelect(
+          "page.userProfile", 
+          "user-profile"
+        )
+        .where("user-profile.penName = :penName OR user-profile.id = :id", 
+          { 
+            penName, 
+            id
+          }
+        )
+        .andWhere("page.disclosureScope >= :scope", { scope: 0 })
+        .select("page.id")
+        .addSelect("page.title")
+        .addSelect("page.disclosureScope")
+        .getMany();
 
       if(pageList[0]) {
-        return pageList.map((page) => {
-          return {
-            pageId: page.id,
-            title: page.title,
-            disclosureScope: page.disclosureScope
-          }
-        });
+        return pageList;
       }
     } 
 
