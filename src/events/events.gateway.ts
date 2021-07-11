26b5f332898eda84ab67907755e4ab8605251e90
junhee <wjunhee102@gmail.com>
2021-07-12 02:01:07 +0900

    #4 [feat] 수정중인 블럭의 사용자 정보 전송 기능 추가

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -1,9 +1,12 @@
 import { Logger } from "@nestjs/common";
 import { IoAdapter } from "@nestjs/platform-socket.io";
 import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from "@nestjs/websockets";
-import { from, Observable } from "rxjs";
-import { map } from "rxjs/operators";
 import { Server, Socket } from "socket.io";
+
+interface EditingUserInfo {
+  penName: string;
+  editingId: string | null;
+}
 
 @WebSocketGateway(4600, {
   namespace: "bklog"
@@ -28,9 +31,13 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
   }
 
   @SubscribeMessage('roomjoin')
-  handleEvent(client: Socket, data: string) {
-    client.join(data, () => { this.logger.log(`join ${client.id}`) });
-    client.to(data).emit("message", "hello");
+  handleJoin(client: Socket, roomId: string) {
+    client.join(roomId, () => { this.logger.log(`join ${client.id}`) });
+  }
+
+  @SubscribeMessage('roomleave')
+  handleLeave(client: Socket, roomId: string) {
+    client.leave(roomId, () => { this.logger.log(`leave ${client.id}`) });
   }
 
   @SubscribeMessage('update')
@@ -38,8 +45,14 @@ export class EventsGateway implements OnGatewayInit, OnGatewayConnection, OnGate
     client.to(data[0]).emit("updated", data[1]);
   }
 
-  @SubscribeMessage('identity')
-  async identity(@MessageBody() data: number): Promise<number> {
-    return data;
+  @SubscribeMessage('edit')
+  handleEditing(client: Socket, data: [string, EditingUserInfo]) {
+    client.to(data[0]).emit("editing", data[1]);
   }
+
+  @SubscribeMessage('edited')
+  handleEdited(client: Socket, data: [string, string]) {
+    client.to(data[0]).emit("edited", data[1]);
+  }
+
 }
