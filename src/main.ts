975bd8fe665c935f55564a03c9aba0a0e060357a
junhee <wjunhee102@gmail.com>
2021-08-08 16:19:14 +0900

    temp: cors

--- src/main.ts
+++ src/main.ts
@@ -8,22 +8,22 @@ import { SocketIoAdapter } from './adapters/socket-io.adapter';
 async function bootstrap() {
   //undefined는 insomnia 때문
   const whitelist = ['https://bklog-app-deploy.vercel.app', 'http://localhost', 'http://localhost:3000', 'http://localhost:4500', 'https://bklogapi.com', undefined];
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
-  // app.enableCors({
-  //   origin: (origin, callback) => {
-  //     if (whitelist.indexOf(origin) !== -1) {
-  //       console.log("allowed cors for:", origin);
-  //       callback(null, true);
-  //     } else {
-  //       console.log("blocked cors for:", origin);
-  //       callback(new Error('Not allowed by CORS'));
-  //     }
-  //   },
-  //   allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-  //   methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
-  //   credentials: true,
-  // });
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log("allowed cors for:", origin);
+        callback(null, true);
+      } else {
+        console.log("blocked cors for:", origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
+    credentials: true,
+  });
   // app.useGlobalPipes(new ValidationPipe({
   //   whitelist: true,
   //   forbidNonWhitelisted: true,
