a30f990d6b699fd71d13ad8d2163229a24bca21f
junhee <wjunhee102@gmail.com>
2021-08-11 05:02:33 +0900

    #5 fix: firebase url 추가

--- src/main.ts
+++ src/main.ts
@@ -7,25 +7,19 @@ import { SocketIoAdapter } from './adapters/socket-io.adapter';
 
 async function bootstrap() {
   //undefined는 insomnia 때문
-  const whitelist = ['https://bklog-app-deploy.vercel.app', 'http://localhost', 'http://localhost:3000', 'http://localhost:4500', 'https://bklogapi.com', undefined];
+  const whitelist = ['https://bklog-app.web.app', 'https://bklog-app-deploy.vercel.app', 'http://localhost', 'http://localhost:3000', 'http://localhost:4500', 'https://bklogapi.com', undefined];
   const app = await NestFactory.create(AppModule);
 
-  // app.enableCors({
-  //   origin: (origin, callback) => {
-  //     if (whitelist.indexOf(origin) !== -1) {
-  //       console.log("allowed cors for:", origin);
-  //       callback(null, true);
-  //     } else {
-  //       console.log("blocked cors for:", origin);
-  //       callback(new Error('Not allowed by CORS'));
-  //     }
-  //   },
-  //   allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-  //   methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
-  //   credentials: true,
-  // });
   app.enableCors({
-    origin: 'https://bklog-app-deploy.vercel.app',
+    origin: (origin, callback) => {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log("allowed cors for:", origin);
+        callback(null, true);
+      } else {
+        console.log("blocked cors for:", origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
     methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS",
     credentials: true,
