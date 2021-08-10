318eee01057d6c176b478a9122d28f137a6db9fe
junhee <wjunhee102@gmail.com>
2021-08-11 05:51:43 +0900

    #5 temp: origin 변경

--- src/main.ts
+++ src/main.ts
@@ -25,15 +25,7 @@ async function bootstrap() {
   //   credentials: true,
   // });
   app.enableCors({
-    origin: (origin, callback) => {
-      if (whitelist.indexOf(origin) !== -1) {
-        console.log("allowed cors for:", origin);
-        callback(null, true);
-      } else {
-        console.log("blocked cors for:", origin);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: "*",
     allowedHeaders: 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
     methods: "GET,PUT,POST,DELETE,UPDATE,OPTIONS"
   });
