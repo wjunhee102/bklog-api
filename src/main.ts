2c978a1a649f0ab79dd077c74c853d53d37d3534
junhee <wjunhee102@gmail.com>
2021-08-13 03:46:34 +0900

    #5 feat: client url 변경

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { SocketIoAdapter } from './adapters/socket-io.adapter';
 
 async function bootstrap() {
   //undefined는 insomnia 때문
-  const whitelist = ['https://bklog-app.web.app', 'https://bklog-app-deploy.vercel.app', 'http://localhost', 'http://localhost:3000', 'http://localhost:4500', 'https://bklogapi.com', undefined];
+  const whitelist = ['https://bklog-app.web.app', 'https://bklog-app.vercel.app', 'http://localhost', 'http://localhost:3000', 'http://localhost:4500', 'https://bklogapi.com', undefined];
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
