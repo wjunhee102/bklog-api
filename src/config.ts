1fb49bf397f23d1580ddd0507df5d2d60862e1a7
junhee <wjunhee102@gmail.com>
2021-08-20 04:12:03 +0900

    temp: cors

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const setWhitelist = (env: string | undefined) => {
   console.log(env);
   if(env !== "dev") {
-    return ['https://bklog-app.web.app', 'https://bklog-app.vercel.app', 'https://bklog-app.vercel.app/']
+    return ['https://bklog-app.web.app', 'https://bklog-app.vercel.app', 'https://bklog-app.vercel.app/', undefined]
   } else {
     return ['http://localhost', 'http://localhost:3000', 'http://localhost:4500', undefined];
   }
