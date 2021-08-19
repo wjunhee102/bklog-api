5867e612439eacda7e8095c6d8027efa1b15c407
junhee <wjunhee102@gmail.com>
2021-08-19 22:59:13 +0900

    fix: cors 수정

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const setWhitelist = (env: string | undefined) => {
   console.log(env);
-  if(env === "prod") {
+  if(env !== "dev") {
     return ['https://bklog-app.web.app', 'https://bklog-app.vercel.app']
   } else {
     return ['http://localhost', 'http://localhost:3000', 'http://localhost:4500', undefined];
