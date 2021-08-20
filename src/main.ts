433583826d954c575b6440a719a831fd74947ce4
junhee <wjunhee102@gmail.com>
2021-08-21 04:24:45 +0900

    #10 fix: session 설정 - sameSite: none

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
     cookie: {
       httpOnly: true,
       secure: process.env.ENV === "prod"? true : false,
-      sameSite: process.env.ENV === "prod"? true : false
+      sameSite: process.env.ENV === "prod"? 'none' : false
     }
   }))
   .setGlobalPrefix('/v2')
