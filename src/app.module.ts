6ff74f72ecc8ac9a1ada8eb906c06e23845993db
junhee <wjunhee102@gmail.com>
2021-11-27 19:35:03 +0900

    #9 [feat] admin module 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BklogModule } from './bklog/bklog.module';
@@ -8,6 +8,7 @@ import { UserModule } from './user/user.module';
 import { EventsModule } from './events/events.module';
 import ormConfig from './utils/database/orm-config';
 import { ConfigModule } from '@nestjs/config';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
   imports: [
@@ -18,7 +19,8 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
     BklogModule,
     UserModule,
-    EventsModule
+    EventsModule,
+    AdminModule
   ],
   controllers: [AppController],
   providers: [AppService],
